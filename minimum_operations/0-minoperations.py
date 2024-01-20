53979abb1a27a0a6fdd681d2b0e06bf0a13cd4c9
madol-abraham <m.madol@alustudent.com>
2024-01-20 05:26:05 -0800

    updated

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
+"""
+Function that calculates the min operations to copy and paste letters
+"""
 
-'''Minimum Operations'''
-import math
 
-
-def min_operations(n):
-    '''Minimum operations to get n to 1'''
-    if n <= 1:
-        return 0
-
-    operations = 0
-    for i in range(2, int(math.sqrt(abs(n))) + 1):
-        while n % i == 0:
-            operations += i
-            n //= i
-
-    if n > 1:
-        operations += n
-
-    return operations
-
+def minOperations(n):
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
 
